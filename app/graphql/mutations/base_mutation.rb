5a1dae006c581edb4840ea34a6a10b7f86a2a87f
Varsion <jianhual12@outlook.com>
2022-05-17 17:36:41 +0800

    feat: add auth function into base mutation

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -4,5 +4,17 @@ module Mutations
     field_class Types::Base::Field
     input_object_class Types::Base::InputObject
     object_class Types::Base::Object
+
+    protected
+
+    def authenticate_user!
+      raise GraphQL::ExecutionError, "unauthenticated" unless current_user
+    end
+
+    private
+
+    def current_user
+      @current_user ||= context[:user]
+    end
   end
 end
